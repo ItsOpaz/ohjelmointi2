48ac4f457854e44114bc85a607c905e859558155
Olli Pettersson <petterso@linux-desktop14.tuni.fi>
2019-09-09 14:22:51 +0300

    Lisätty 3:lla jaollisten lukujen käsittely

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -9,6 +9,11 @@ int main()
     std::cout << "How many numbers would you like to have? ";
     std::cin >> n;
     while(n>=counter){
+        if (counter%3==0){
+            std::cout << "zip" << std::endl;
+            counter = counter +1;
+
+        }
         std::cout << counter << std::endl;
         counter = counter +1;
 
