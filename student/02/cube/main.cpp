25b092c7bb21c21578073a0a6068854a786712cb
Olli Pettersson <petterso@linux-desktop5.tuni.fi>
2019-09-14 18:36:41 +0300

    korjattu piste tulostetun rivin perään, korjattu arvon tarkistus

--- student/02/cube/main.cpp
+++ student/02/cube/main.cpp
@@ -5,25 +5,33 @@ int main()
 {
     int value;
     int result;
+    int check;
     std::cout << "Enter a number: ";
     std::cin >> value;
     result = pow(value,3);
+    check = pow(result,(float)1/3);
+    std::cout << value;
+    std::cout << result;
+    std::cout << check;
     if (result<0){
         std::cout <<"Error! The cube of ";
         std::cout <<value;
         std::cout << " is not ";
-        std::cout << result << std::endl;
+        std::cout << result;
+        std::cout << "." << std::endl;
     }
-    else if(value != pow(result, 1/3)){
+    else if(value != check){
         std::cout <<"Error! The cube of ";
         std::cout <<value;
         std::cout << " is not ";
-        std::cout << result << std::endl;
+        std::cout << result;
+        std::cout << "." << std::endl;
     }
     else{
-    std::cout <<"The cube of ";
-    std::cout << value;
-    std::cout <<" is ";
-    std::cout << result << std::endl;
+        std::cout <<"The cube of ";
+        std::cout << value;
+        std::cout <<" is ";
+        std::cout << result;
+        std::cout << "." << std::endl;
     }
 }
