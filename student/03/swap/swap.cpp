c856f490f545bfe84a3ed35da4baaaafbc27184b
Olli Pettersson <petterso@linux-desktop5.tuni.fi>
2019-09-20 22:22:48 +0300

    lisätty swap funktion määrittely ja esittely

---
+++ student/03/swap/swap.cpp
@@ -0,0 +1,9 @@
+#include "swap.hh"
+
+// TODO: Implement swap function here
+
+int swap(int& i, int& j) {
+    int a = i;
+    i=j;
+    j=a;
+}
---
+++ student/03/swap/swap.hh
@@ -0,0 +1,3 @@
+// TODO: Write the declaration of swap function here
+int swap(int& i, int& j);
+
