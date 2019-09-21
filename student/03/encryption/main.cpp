f5f10bafb858c0fcaf505f7e25adcc711814a304
Olli Pettersson <petterso@linux-desktop12.tuni.fi>
2019-09-21 23:04:42 +0300

    fixed whole code

--- student/03/encryption/main.cpp
+++ student/03/encryption/main.cpp
@@ -1,57 +1,58 @@
 #include <iostream>
-#include <string>
 #include <cctype>
-#include <stdio.h>
 
 using namespace std;
 
+const string ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+bool key_check(string key)
+{
+    string::size_type i = 0;
+    i = key.length();
+    if (i != 26) {
+        cout << "Error! The encryption key must contain 26 characters." << endl;
+        return false;
+    }
+    for (int k = 0; k <= 25; ++k) {
+        if (islower(key[k]) == false){
+            cout << "Error! The encryption key must contain only lower case characters." << endl;
+            return false;
+        }
+    }
+    for (int l = 97; l <= 122; ++l) {
+        if (key.find(l) == string::npos) {
+            cout << "Error! The encryption key must contain all alphabets a-z." << endl;
+            return false;
+        }
+    }
+    return true;
+}
+
+string encrypt(string input_text, string key)
+{
+    int input_length;
+    input_length = input_text.length();
+    int index;
+    string encrypted_text = input_text;
+    for (int k = 0; k <= input_length-1; ++k) {
+        index = ALPHABET.find(input_text.at(k));
+        encrypted_text.at(k) = key.at(index);
+    }
+    return encrypted_text;
+}
+
 int main()
 {
-    string line;
-    string base = "abcdefghijklmnopqrstuvwzyx";
     cout << "Enter the encryption key: ";
-    cin >> line;
-    if (line.length()==26){
-        for (int i=1; i < 26; i++){
-            if (isupper(line.at(i))){
-                cout << "Error! The encryption key must contain only lower case characters."<< endl;
-                return EXIT_FAILURE;
-            }
-        }
-        int j;
-        int count[26] = {0};
-
-        for(j=0;line[j]!='\0';j++){
-        count[line[j]-'a']++;
-        }
-        for(j=0;j<26;j++){
-            if(!count[j]){
-                cout << "Error! The encryption key must contain all alphabets a-z." << endl;
-                return EXIT_FAILURE;
-            }
-        }
-        string encrypt;
-        cout << "Enter the text to be encrypted: ";
-        cin >> encrypt;
-        for (int i=0; i < 26; i++){
-            if (isupper(encrypt.at(i))){
-                cout << "Error! The encryption key must contain only lower case characters."<< endl;
-                return EXIT_FAILURE;
-            }
-        }
-        int input_length;
-        input_length = encrypt.length();
-        int index;
-        string crypted_text = encrypt;
-        for (int i=0; i < input_length-1 ;++i ){
-            index = base.find(encrypt.at(i));
-            crypted_text.at(i) = line.at(index);
-                }
-        cout << "Encrypted text: " << crypted_text << endl;
-
-    }else{
-        cout << "Error! The encryption key must contain 26 characters."<< endl;
+    string key;
+    cin >> key;
+    if (key_check(key) == false)
         return EXIT_FAILURE;
+    else {
+        cout << "Enter the text to be encrypted: ";
+        string input_text;
+        cin >> input_text;
+        cout << "Encrypted text: " << encrypt(input_text, key) << endl;
     }
 
 }
