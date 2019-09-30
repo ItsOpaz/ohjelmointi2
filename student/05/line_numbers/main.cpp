65b2034d7161a8251454372a61846f9b00ee4704
Pete <olli.pettersson@tuni.fi>
2019-09-30 14:48:23 +0300

    first submission

---
+++ student/05/line_numbers/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    string inp;
+    string outp;
+    cout << "Input file: ";
+    cin >> inp;
+    cout << "Output file: ";
+    cin >> outp;
+    ifstream tiedosto_in(inp);
+        if ( not tiedosto_in ) {
+            cout << "Error! The file " << inp << " cannot be opened." << endl;
+            return EXIT_FAILURE;
+        }
+        else{
+            ofstream tiedosto_out(outp);
+            string rivi;
+            int i = 1;
+                    while ( getline(tiedosto_in, rivi) ) {
+                        tiedosto_out << i << " " << rivi << endl;
+                    }
+                    tiedosto_in.close();
+                    tiedosto_out.close();
+        }
+}
